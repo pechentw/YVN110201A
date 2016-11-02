b48f341d5f25ff587f89f1ef62ee379389c21f98
pechentw <pechen.tw@gmail.com>
2016-11-02 20:41:44 +0800

    button layout

--- app/src/main/java/com/soho/yvtc/yvn110201a/MainActivity.java
+++ app/src/main/java/com/soho/yvtc/yvn110201a/MainActivity.java
@@ -2,6 +2,8 @@ package com.soho.yvtc.yvn110201a;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Button btn = new Button(MainActivity.this);
+//        setContentView(btn);
+        Button btn = new Button(MainActivity.this);
+        btn.setText("Click!!");
+        Button btn2 = new Button(MainActivity.this);
+        btn2.setText("Click2222!!");
+        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_main);
+        layout.addView(btn);
+        layout.addView(btn2);
+
     }
 }
